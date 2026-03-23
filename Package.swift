fd9296ebe86b35fa8f8bda40098cace2588339fa
kyungboshim <kyungbo.shim@neowiz.com>
2026-03-23 14:13:37 +0900

    5.2.3 배포

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdiscopeCore",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.2/Adiscope.zip",
-            checksum: "5ec4d790196285d7d3efbd378c12f97301950d441f59b5c9ca4b28b03086c5c5"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.3/Adiscope.zip",
+            checksum: "669b305c4abc981b8414b64ae2a4816c1d2d5258b24a04956fdf8085f9feeefc"
         ),
     ]
 )
